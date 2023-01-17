d1e39aebdec163b8dcc10afbce669c2abb974687
FelixvL <digitaleservice@gmail.com>
2023-01-17 16:08:52 +0100

    AB

--- Demo.java
+++ Demo.java
@@ -1,7 +1,19 @@
-class Demo{   // AA
+class Demo{   // AB
 	public static void main(String args[]){
 		System.out.println("Hij doet het");
+		Voeding v = new Voeding();
+		Voeding r = new Voeding();
+		v.prijs = 25;
+		System.out.println(v.prijs);
+		r = v;
+		System.out.println(r.prijs);
 	}
+}
 
+class Voeding{
+	int prijs;
+	Voeding(){
+		System.out.println("Geoogst");
+	}
 
 }
