bab3afb110dda7d4034889dcfe329f3ebf739865
FelixvL <digitaleservice@gmail.com>
2023-02-16 11:16:52 +0100

    TH

--- Demo.java
+++ Demo.java
@@ -1,24 +1,28 @@
-class Demo{     // TG
+class Demo{     // TH
 	public static void main(String[] args){
 		System.out.println("maan");
-		wandelen();
-		System.out.println("zon");
-	}
-	static void wandelen(){
-		System.out.println("wandelen");
 		Fiets f = new MTB();
 		System.out.println(f.speed);
+		f.fietsen();
+		System.out.println("zon");
 	}
 }
 class Fiets{
-	int speed;
+	int speed = 3;
 	Fiets(){
 		System.out.println("Fiets");
 	}
+	void fietsen(){
+		System.out.println("f in fiets");
+	}
 }
 class MTB extends Fiets{
-	class MTB(){
+	int speed = 9;
+	MTB(){
 		System.out.println("MTB");
+	}
+	void fietsen(){
+		System.out.println("f in mtb");
 	}
 }
 
