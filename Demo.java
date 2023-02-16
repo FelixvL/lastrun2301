8cf169e2751410569124f82ded6147465cba05cd
FelixvL <digitaleservice@gmail.com>
2023-02-16 10:02:16 +0100

    TESTJE

--- Demo.java
+++ Demo.java
@@ -1,26 +1,6 @@
-class Demo{   // BF
-	public static void main(String args[]){
-		Vraag c = new StrikVraag();
-		c.lopen();
-		System.out.println(c.a);
-		c.go();
-	}
-}
-abstract class Vraag{
-	int a = 3;
-	public void lopen(){
+class Demo{   
+	public static void main(String[] args){
 		System.out.println("hoi");
-	}
-	abstract void go();
-	
-}
-class StrikVraag extends Vraag{
-	int a = 4;
-	void lopen(){
-		System.out.println("doei");
-	}
-	public void go(){
-		System.out.println("Go");
 	}
 }
 
