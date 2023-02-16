c4fad921fbc5d39bf0a1c9e9d082e919064fcc26
FelixvL <digitaleservice@gmail.com>
2023-02-16 10:19:18 +0100

    TA

--- Demo.java
+++ Demo.java
@@ -1,6 +1,9 @@
-class Demo{   
+class Demo{     // TA
 	public static void main(String[] args){
 		System.out.println("hoi");
+		float f = 23.3;
+		System.out.println(f);
+		System.out.println("Einde "+ 4 + 4);
 	}
 }
 
